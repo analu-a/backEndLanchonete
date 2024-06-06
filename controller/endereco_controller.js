91d3d4ed8a3ea3fb5225495a9b536cf28ff9dd81
vih-az <124360742+vih-az@users.noreply.github.com>
2024-06-06 01:40:47 -0300

    Update endereco_controller.js

--- controller/endereco_controller.js
+++ controller/endereco_controller.js
@@ -1,3 +1,15 @@
-const enderecoModelDAO = require("../model/DAO/endereco.js")
+const enderecosModelDAO = require("../model/DAO/endereco.js")
+const enderecosModuloConfig = require("../modulo/config.js")
 const pegarEnderecos = async function(){
-  
+  let enderecosJSON = {}
+  const resultadoEnderecos = await enderecosModelDAO.pegarEnderecos()
+  if(resultadoEnderecos){
+    if(resultadoEnderecos.length > 0){
+      enderecosJSON.enderecos = resultadoEnderecos
+      enderecosJSON.quantidade = resultadoEnderecos.lenght
+      enderecosJSON.status = 200
+    }else{
+      return enderecosModuloConfig.ERROR_NOT_FOUND
+    }
+  }else{
+    return enderecosModuloConfig.ERROR_
